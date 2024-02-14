fa938995f0df5263957100040896ffbb29c0ddbc
Luan <luam10@hotmail.com.br>
2024-02-13 22:55:03 -0300

    finalizado

--- JS/script.js
+++ JS/script.js
@@ -16,4 +16,47 @@ $(document).ready(function(){
     $(xxxxx).mask('SSS-0000',{placeholder: 'ABC-1111'})
     
     */
+
+    $('form').validate({
+        rules:{
+            nome: {
+                required: true
+            }, email: {
+                required: true,
+                email:true
+            }, telefone: {
+                required: true
+            },
+            menssagem: {
+                required: true
+            },
+            veiculo: {
+                required:  false
+            }
+        },
+        menssages: {
+            nome: 'Por favor, insira o seu nome'
+        },
+        submitHandler: function(form){
+            console.log(form)
+        },
+        invalidHandler: function(e, validador){
+            let camposIncorretos = validador.numberOfInvalids()
+            if(camposIncorretos){
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+
+    $('.lista-carro button').click(function(){
+        const destino = $('#contato')
+        const veiculo =  $(this).parent().find('h3').text()
+
+
+        $('#veiculo-interesse').val(veiculo)
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 1000)
+    })
 })
